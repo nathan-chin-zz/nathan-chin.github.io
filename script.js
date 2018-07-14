2b021b0a3e7f472a42146297379b6bb5be65f245
nathan-chin <nathanchin@utexas.edu>
2018-07-14 02:36:45 -0500

    Fix hover error

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 /*global $, jQuery, alert*/
 
 $(document).ready(function () {
+    $('.message').toggleClass('hidden');
     $('.hover').hover(function () {
         $('.message').toggleClass('hidden');
     });
